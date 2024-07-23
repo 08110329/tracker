d8274dcedd6307685b6417730e5be2ebaab94a40
24LU8176 24LU8176 <24LU8176@G301-CC0203.local>
2024-07-23 22:09:16 +0800

    m

--- src/components/Contentjs.js
+++ src/components/Contentjs.js
@@ -3,6 +3,9 @@ import { Checkbox } from "./ui/checkbox";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { RadioGroup } from "./ui/radio-group";
+import { Select } from "./ui/select";
+import { Eye } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 export const Contentjs = () => {
   return (
@@ -22,20 +25,42 @@ export const Contentjs = () => {
           <h1>Types</h1>
           <div className="grid ">
             <div className="flex gap-2 px-3	py-3">
-              <Checkbox id="terms" className="rounded-lg"/>
-              <Label htmlFor="terms" className="text-base font-normal">All</Label>
+              <Checkbox id="terms" className="rounded-lg" />
+              <Label htmlFor="terms" className="text-base font-normal">
+                All
+              </Label>
             </div>
             <div className="flex gap-2 px-3	py-3">
-              <Checkbox id="terms" className="rounded-lg"/>
-              <Label htmlFor="terms" className="text-base font-normal">Income</Label>
+              <Checkbox id="terms" className="rounded-lg" />
+              <Label htmlFor="terms" className="text-base font-normal">
+                Income
+              </Label>
             </div>
             <div className="flex gap-2 px-3	py-3">
-              <Checkbox id="terms" className="rounded-lg "/>
-              <Label htmlFor="terms" className="text-base font-normal">Expense</Label>
+              <Checkbox id="terms" className="rounded-lg " />
+              <Label htmlFor="terms" className="text-base font-normal">
+                Expense
+              </Label>
             </div>
           </div>
         </div>
-        <div></div>
+        <div className="grid gap-4">
+          <div className="flex justify-between">
+            <h1 className="font-semibold text-base">Category</h1>
+            <p>Clear</p>
+          </div>
+          <div className="grid gap-2">
+            <div className="flex justify-between px-3	">
+              <div className="flex gap-1">
+                <Eye className="w-[20px] h-[20px]"/>
+                <p className="text-base	font-normal	">Food & Drinks</p>
+              </div>
+              <p>
+                <ChevronRight />
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
---
+++ src/components/Selectjs.js
@@ -0,0 +1,15 @@
+export const Selectjs = () => {
+    return (
+        <div className="grid gap-2">
+            <div className="flex justify-between px-3	">
+              <div className="flex gap-1">
+                <Eye className="w-[20px] h-[20px]"/>
+                <p className="text-base	font-normal	">Food & Drinks</p>
+              </div>
+              <p>
+                <ChevronRight />
+              </p>
+            </div>
+          </div>
+    )
+}
